7d64057cdc4ce763caafb4d0e9eea46c6ee8049c
Matt White <matt@matthewjwhite.co.uk>
2026-02-15 15:43:50 +0000

    tweak variables to get code to run

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,11 @@ variable "container_registry_username" {
 variable "container_registry_password" {
   type      = string
   sensitive = true
+
+  validation {
+    condition     = length(var.container_registry_password) > 0
+    error_message = "container_registry_password must be provided and not empty."
+  }
 }
 
 variable "image_tag" {
